8b5c08994cbaa7c0d3e76c1ba8ba141129bfebde
Yang Ge <ygee0015@student.monash.edu>
2021-08-18 11:58:11 +0800

    Week3

---
+++ Week3/FIT5032_MyCodeSnippet/FIT5032_MyCodeSnippet/Models/Exercise/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_MyCodeSnippet.Models.Exercise
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Student(String name, String phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
