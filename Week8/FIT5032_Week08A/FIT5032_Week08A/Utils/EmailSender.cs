5e4d6885fdf8e7c4ad2a4bebc8188d72f1f65640
YangGe2513 <88759153+YangGe2513@users.noreply.github.com>
2021-09-17 19:00:26 +0800

    Week8

---
+++ Week8/FIT5032_Week08A/FIT5032_Week08A/Utils/EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("ygee0015@student.monash.edu", "FIT5032 Example Email User");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
